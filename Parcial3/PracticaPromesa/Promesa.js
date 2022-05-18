4f29328406eab87e2c46574526cbf4641ba77b13
FranciscoDRG <pancho221299@gmail.com>
2022-05-18 14:46:34 -0500

    Practica promesa

---
+++ Parcial3/PracticaPromesa/Promesa.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    $('#botonAlerta').click(function() {
+        $('#ventanaAlerta').show();
+    });
+    $('#botonCerrarAlerta').click(function(){
+        $('#ventanaAlerta').hide();
+    });
+    $('#botonAjax').click(function(){
+        /*var solicitud = new XMLHttpRequest();          //Crear objeto XMLHttpRequest
+        solicitud.onreadystatechange = function() {   // En esta propiedad declaramos la funcion a ejecutar
+                                                    // cuando cambie el status del objeto XMLHttpRequest
+            if (solicitud.readyState == 4 && solicitud.status == 200) {                 // La respueta esta lista
+                document.getElementById("head").innerHTML = solicitud.responseText;  // La propiedad responseText tiene la respuesta en texto  
+            }};
+
+        solicitud.open("GET", "header.txt", true);
+        solicitud.send(); */
+        new Promise(function(resolve,reject){
+            var solicitud = new XMLHttpRequest();
+            solicitud.onreadystatechange = function(){
+                if (solicitud.readyState == 4 && solicitud.status == 200){
+                    resolve(solicitud.responseText);
+                }
+            };
+            solicitud.open("GET","header.txt",true);
+            solicitud.send();
+        })
+        .then (value => document.getElementById("head").innerHTML = value);
+    })
+    $('#botonRegreso').click(function(){
+        document.getElementById("head").innerHTML = "<h1>Formulario de registro (beca Tamaulipas)</h1>"; 
+    });
+    $('#btnjson').click(function(){
+        $.post('registro.php',{},function(data){
+            console.log(data);
+            $('#idCliente').val(data.idCliente);
+            $('#nomCliente').val(data.nomCliente);
+            $('#hora').val(data.hora);
+            $('#dirCliente').val(data.dirCliente);
+            $('#telCliente').val(data.telCliente);
+            $('#ciudad').val(data.ciudad);
+            $('#estado').val(data.estado);
+            $('#pais').val(data.pais);
+        },'json');
+    });
+});
